6231a7ea78ba5d63a124dc151d96f601efe8a667
Vaishnavi Muttamsetty <91942146+muttamsetty@users.noreply.github.com>
2022-03-04 15:42:08 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "test" {
+  #you can see amiid's in launch instance aws portal, note ami ids are different from each region
   ami               = "ami-08b6f2a5c291246a0"
   availability_zone = "us-east-1a"
   instance_type     = "t2.micro"
